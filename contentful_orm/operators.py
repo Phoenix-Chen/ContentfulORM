cd959739b1c1b3dcf7e98d56860fe0ed41b561f0
Phoenix Chen <phoenix0722chen@gmail.com>
2019-08-27 15:18:26 -0700

    Add operators ne_, nin_, exists_, lt_, lte_, gt_, gte_, match_, near_ and within_

--- contentful_orm/operators.py
+++ contentful_orm/operators.py
@@ -6,3 +6,33 @@ def query_factory(operator=None, **kwargs):
 
 def all_(**kwargs):
     return query_factory(operator='all', **kwargs)
+
+def ne_(**kwargs):
+    return query_factory(operator='ne', **kwargs)
+
+def nin_(**kwargs):
+    return query_factory(operator='nin', **kwargs)
+
+def exists_(**kwargs):
+    return query_factory(operator='exists', **kwargs)
+
+def lt_(**kwargs):
+    return query_factory(operator='lt', **kwargs)
+
+def lte_(**kwargs):
+    return query_factory(operator='lte', **kwargs)
+
+def gt_(**kwargs):
+    return query_factory(operator='gt', **kwargs)
+
+def gte_(**kwargs):
+    return query_factory(operator='gte', **kwargs)
+
+def match_(**kwargs):
+    return query_factory(operator='match', **kwargs)
+
+def near_(**kwargs):
+    return query_factory(operator='near', **kwargs)
+
+def within_(**kwargs):
+    return query_factory(operator='within', **kwargs)
